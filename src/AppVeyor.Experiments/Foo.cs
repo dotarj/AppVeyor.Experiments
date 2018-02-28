8b86eba84dc1018a0fa0824a4548d5a2831bca3f
Arjen Post <mail@arjenpost.nl>
2018-02-28 12:51:38 +0100

    project setup

---
+++ src/AppVeyor.Experiments/Foo.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Arjen Post. See LICENSE in the project root for license information.
+
+namespace AppVeyor.Experiments
+{
+    /// <summary>
+    /// Provides a method for testing purposes.
+    /// </summary>
+    public static class Foo
+    {
+        /// <summary>
+        /// Returns the given <paramref name="value"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The given <paramref name="value"/>.</returns>
+        public static string Bar(string value)
+        {
+            return value;
+        }
+    }
+}
---
+++ test/AppVeyor.Experiments.Tests/FooTests.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Arjen Post. See LICENSE in the project root for license information.
+
+using Xunit;
+
+namespace AppVeyor.Experiments.Tests
+{
+    public class FooTests
+    {
+        [Fact]
+        public void TheBarMethodShouldReturnValue()
+        {
+            // Arrange
+            var value = "foo";
+
+            // Act
+            var result = Foo.Bar(value);
+
+            // Assert
+            Assert.Equal(value, result);
+        }
+    }
+}
