dd9c7b4f6d54a0a7bb950d30aa11c71f356d2802
Arjen Post <mail@arjenpost.nl>
2018-03-01 07:57:02 +0100

    restore Foo.Bar implementation

--- src/AppVeyor.Experiments/Foo.cs
+++ src/AppVeyor.Experiments/Foo.cs
@@ -1,6 +1,4 @@
 ﻿// Copyright (c) Arjen Post. See LICENSE in the project root for license information.
-using System.Linq;
-
 namespace AppVeyor.Experiments
 {
     /// <summary>
@@ -15,7 +13,7 @@ namespace AppVeyor.Experiments
         /// <returns>The given <paramref name="value"/>.</returns>
         public static string Bar(string value)
         {
-            return new string(value.Reverse().ToArray());
+            return value;
         }
     }
 }
